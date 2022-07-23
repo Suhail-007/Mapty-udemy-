3f0545f709169af2706549393ec14d2c213d1926
Suhail-007 <suhuq007@gmail.com>
2022-07-23 13:24:21 +0530

    added leaflet map

--- script.js
+++ script.js
@@ -12,12 +12,25 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 //get Coordinates
-if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(
+		function showPos () {
+			navigator.geolocation.getCurrentPosition(
 		function (position) {
-		console.log(position);
+		const {latitude} = position.coords;
+		const {longitude} = position.coords;
+		const coords = [latitude, longitude];
+		
+		const map = L.map('map').setView(coords, 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
 }, function () {
 		alert('couldn\'t get your location')
 		}
-		)
+)	
 }
+
+showPos();
